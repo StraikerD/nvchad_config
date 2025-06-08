81dbe15389db3f5bc76e079c6c18b4b39f8c1e37
Samurai Jack <l1evilone@gmail.com>
2025-06-08 04:54:35 +0300

    add ftplugin for C/C++ filetypes

---
+++ ftplugin/c.lua
@@ -0,0 +1,27 @@
+vim.bo.expandtab = true
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.textwidth = 101
+vim.wo.colorcolumn = '+0'
+
+vim.opt_local.listchars = { leadmultispace = '│   ' }
+
+local function files_exist(names)
+  local filename = ""
+  for _,v in ipairs(names) do
+    filename = vim.fn.expand("%:p:h").."/"..v
+    if vim.fn.filereadable(filename) == 1 then
+      return true
+    end
+  end
+  return false
+end
+
+if not files_exist({ "Makefile", "CMakeLists.txt" }) then
+  if vim.bo.filetype == "c" then
+    vim.bo.makeprg = "gcc % -o %< && ./%<"
+  elseif vim.bo.filetype == "cpp" or vim.bo.filetype == "cxx" then
+    vim.bo.makeprg = "g++ % -o %< && ./%<"
+  end
+end
+
--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,40 +3,12 @@ require "nvchad.autocmds"
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-local function contains (val, tab)
-  for _, value in ipairs(tab) do
-    if value == val then
-      return true
-    end
-  end
-
-  return false
-end
-
-autocmd({"BufWinEnter"}, {
-  desc = "Identation settings",
-  group = augroup("Identation", { clear = true }),
-
-  callback = function()
-    local ftypes = { "c", "h", "cpp", "cxx", "hpp", "hxx" }
-    -- override expandtab set by ftplugins
-    vim.bo.expandtab = vim.go.expandtab
-
-    if contains(vim.bo.filetype, ftypes) then
-      vim.opt_local.listchars["leadmultispace"] ="│   "
-      vim.bo.tabstop = 4
-      vim.bo.shiftwidth = 4
-    end
-  end
-})
-
 autocmd({"VimEnter"}, {
   group = augroup("WorkingDirectory", { clear = true }),
   pattern = {"*.*"},
   callback = function()
-    local path = vim.fn.expand('%:h')..'/'
-    path = "cd "..path
-    vim.api.nvim_command(path)
+    local path = vim.fn.expand('%:h').."/"
+    vim.api.nvim_command("cd "..path)
   end,
 })
 
